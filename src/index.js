c13708f35a83773289a00ca98ad5f7f5813a7619
Esio Freitas <esio.gustavo@gmail.com>
2019-10-21 20:54:20 -0300

    Config sequelize

--- src/index.js
+++ src/index.js
@@ -1,32 +1,4 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
-const path = require("path");
+const server = require("./server");
 
-class App {
-  constructor() {
-    this.express = express();
-    this.isDev = process.env.NODE_ENV !== "production";
+server.listen(process.env.PORT || 3000);
 
-    this.middllewares();
-    this.views();
-    this.routes();
-  }
-
-  middllewares() {
-    this.express.use(express.urlencoded({ extended: false }));
-  }
-
-  views() {
-    nunjucks.configure(path.resolve(__dirname, "app", "views"), {
-      watch: this.isDev,
-      express: this.express,
-      autoescape: true
-    });
-
-    this.express.set("view engine", "njk");
-  }
-
-  routes() {}
-}
-
-module.exports = new App().express;
